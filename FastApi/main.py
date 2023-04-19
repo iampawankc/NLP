b0fa8e27b19f22905a3f746f59833f727f73300a
pawankc7 <pawan_21@live.com>
2023-04-19 20:11:45 +0530

    fastApi

---
+++ FastApi/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+#For JSON objects
+from pydantic import BaseModel
+from typing import Optional
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: int
+    tax: Optional[float] = None
+
+app = FastAPI()
+
+@app.get("/")
+def getAllValues():
+    return {"message": "Hello World"}
+
+@app.get("/param/{param_name}")
+def getAllValuesByParam(param_name:str):
+    #Data validation Handling With pydantic
+    return {"message",param_name}
+
+#Using pydantic to declare JSON objects
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
